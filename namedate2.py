cd2bbd9bc22ce5903db928b36bda4b7facc98b34
BrendanB2 <BrendanB2@users.noreply.github.com>
2022-10-17 15:04:09 +0000

    Learning about input function

---
+++ namedate2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def main():
+
+    user_input = input("Please enter your name:")
+    user_day = input("Please enter day of week:")
+    print("Hello, ",user_input,"! Happy ",user_day,"!",sep="",end="")
+
+main()
